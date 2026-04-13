8717695bfa7578da0d27cf4eac5ad0f462c84f3d
yiidtw <yiidtw@gmail.com>
2026-04-13 15:34:36 +0800

    Add KFA tip widget to docs
    Load tip.js via wrapper script with dark theme, pointing to
    0x9C461A35FB6F48698E559Db402e9b0f5d08c1979 for Base USDC tips.

---
+++ docs/src/tip-loader.js
@@ -0,0 +1,9 @@
+(function() {
+  var s = document.createElement("script");
+  s.src = "https://aide.sh/tip.js";
+  s.setAttribute("data-wallet", "0x9C461A35FB6F48698E559Db402e9b0f5d08c1979");
+  s.setAttribute("data-amounts", "1,2,5");
+  s.setAttribute("data-theme", "dark");
+  s.setAttribute("data-label", "Tip");
+  document.body.appendChild(s);
+})();
